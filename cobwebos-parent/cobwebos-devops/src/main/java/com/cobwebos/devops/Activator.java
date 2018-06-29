29698ad8a2c99b811324f5d224ff65584fd1b550
ipcloudos <ipcloudos@163.com>
2018-06-30 00:44:05 +0800

    add devops process

--- cobwebos-parent/cobwebos-devops/src/main/java/com/cobwebos/devops/Activator.java
+++ cobwebos-parent/cobwebos-devops/src/main/java/com/cobwebos/devops/Activator.java
@@ -16,17 +16,56 @@
  */
 package com.cobwebos.devops;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
-    public void start(BundleContext context) {
-        System.out.println("Starting the devops bundle");
-    }
+	public void start(BundleContext context) {
+		System.out.println("Starting the devops bundle");
+		doStartJenkins();
+	}
 
-    public void stop(BundleContext context) {
-        System.out.println("Stopping the devops bundle");
-    }
+	public void stop(BundleContext context) {
+		System.out.println("Stopping the devops bundle");
+		doStopJenkins();
+	}
+
+	public void doStartJenkins() {
+		System.out.println("Starting the devops platform");
+		String command = "./bin/start_devops.sh";
+		doCmd(command);
+		
+	}
+
+	public void doStopJenkins() {
+		System.out.println("Stopping the devops platform");
+		String command = "./bin/stop_devops.sh";
+		doCmd(command);
+
+	}
+	
+	public void doCmd(String command) {
+		
+		Runtime runtime = Runtime.getRuntime();
+		String line =null;
+		try {
+			Process p = runtime.exec(command);
+			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			while((line=br.readLine())!=null) {
+				System.out.println("cli>"+line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 
 }
