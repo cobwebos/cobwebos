7bbda4e6c9dcce26dc0ea87040097d43acb48147
songyuanyuan <songyuanyuan@raisecom.com>
2018-08-01 15:58:15 +0800

    add restconf

--- cobwebos-parent/cobwebos-restconf/src/main/java/com/cobwebos/restconf/Activator.java
+++ cobwebos-parent/cobwebos-restconf/src/main/java/com/cobwebos/restconf/Activator.java
@@ -16,17 +16,57 @@
  */
 package com.cobwebos.restconf;
 
+import java.net.URI;
+
+import org.glassfish.jersey.netty.httpserver.NettyHttpContainerProvider;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import io.netty.channel.Channel;
+
 public class Activator implements BundleActivator {
+    
+    public static final String ROOT_PATH = "cobwebos/restconf";
+	private static final URI BASE_URI = URI.create("http://localhost:20000/");
+	private Thread thread = null;
 
-    public void start(BundleContext context) {
-        System.out.println("Starting the restconf bundle");
-    }
+	public void start(BundleContext context) {
+		 System.out.println("Starting the restconf bundle");
+		initRestService();
+	}
 
-    public void stop(BundleContext context) {
-        System.out.println("Stopping the restconf bundle");
-    }
+	public void stop(BundleContext context) {
+		 System.out.println("Stopping the restconf bundle");
+		if (thread.isAlive()) {
+			thread.interrupt();
+		}
+	}
+
+	public void initRestService() {
+		System.out.println("init cobwebos restconf server...");
+		ResourceConfig resourceConfig = new ResourceConfig(RestfulResources.class);
+		final Channel server = NettyHttpContainerProvider.createHttp2Server(BASE_URI, resourceConfig, null);
+		thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				server.close();
+			}
+		});
+
+		Runtime.getRuntime().addShutdownHook(thread);
+		System.out.println(String.format(
+				"cobwebos restconf started. (HTTP/2 enabled!)\nTry out %s%s\nStop the cobwebos restconf  using " + "CTRL+C.",
+				BASE_URI, ROOT_PATH));
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("init cobwebos restconf server end.");
+	}
+
 
 }
