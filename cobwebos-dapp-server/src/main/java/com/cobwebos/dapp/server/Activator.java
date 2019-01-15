402d5bbe6ad1f4e46c06b07e31e06a3851a26c62
cobwebos@163.com <cobwebos@163.com>
2019-01-16 00:45:06 +0800

    提交初始化日志

---
+++ cobwebos-dapp-server/src/main/java/com/cobwebos/dapp/server/Activator.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cobwebos.dapp.server;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Activator implements BundleActivator {
+    private static final Logger log = LoggerFactory.getLogger("dapp-server");
+
+    public void start(BundleContext context) {
+        log.info("Starting the dapp-server bundle...");
+        System.out.println("Starting the bundle");
+        log.info("Starting the dapp-server bundle end...");
+    }
+
+    public void stop(BundleContext context) {
+        log.info("Stopping the dapp-server bundle...");
+        System.out.println("Stopping the bundle");
+        log.info("Stopping the dapp-server bundle end...");
+    }
+
+}
